56ef72c6555d9c07aa5e48fa922de2352bbafb88
raehat <raehatsinghnanda@gmail.com>
2024-01-12 02:27:16 +0530

    add hello function

--- app.py
+++ app.py
@@ -44,6 +44,10 @@ def predict():
         return jsonify(result)
     except Exception as e:
         return jsonify({'error': str(e)})
+    
+@app.route('/')
+def hello():
+    return 'Hello'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
